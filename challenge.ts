2abddc17509c60cfc3616dab8ddc5c10aab462ab
ericp84 <ericpaoli65@gmail.com>
2022-09-19 16:34:52 +0200

    correct comments

--- challenge.ts
+++ challenge.ts
@@ -10,7 +10,10 @@ const prettyPrintWilder = (users: User[]) => {
       console.log(`${user.name} is ${user.age} years old`);
     });
   };
-  
+  /**
+   * for user2 we can put optional operator for age and birthday or replace birthday by age and remove it.
+   * it will depend if it's a required field or not
+   * */
   const wilders: User[] = [];
   const user1 = { name: "Pierre", age: 23 };
   const user2 = { name: "Paul", birthday: "10/02/1990", age: 32 };
